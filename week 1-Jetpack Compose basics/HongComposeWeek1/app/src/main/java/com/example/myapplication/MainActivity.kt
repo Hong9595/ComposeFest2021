dc57bbaf2fb7aa5c4fb29301a6930870338eaf22
Hong9595 <ahk959595@gmail.com>
2021-11-14 21:10:04 +0900

    Animating your list

--- week 1-Jetpack Compose basics/HongComposeWeek1/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ week 1-Jetpack Compose basics/HongComposeWeek1/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,7 +3,12 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
@@ -36,11 +41,7 @@ private fun MyApp(names: List<String> = listOf("World", "Compose")) {
     if (shouldShowOnboarding) {
         OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
     } else {
-        Column {
-            for (name in names) {
-                Greeting(name = name)
-            }
-        }
+        Greetings()
     }
 }
 
@@ -68,9 +69,15 @@ fun Greeting(name: String) {
     // mutableStateOf를 사용해 Recomposition을 할 수 있게 추적.
     // Composable은 Recomposition은 언제든지 호출될 수 있기에, 그때마다 초기화가 아닌 기존 값을 유지하기 위해 remember 사용.
     //  - 각 Composable마다 지역변수 개념으로 가지게 된다는 걸 기억하자.
-    val expanded = remember { mutableStateOf(false) }
+    var expanded by remember { mutableStateOf(false) }
 
-    val extraPadding = if (expanded.value) 48.dp else 0.dp
+    val extraPadding by animateDpAsState(
+        if (expanded) 48.dp else 0.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        )
+    )
     Surface(
         color = MaterialTheme.colors.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
@@ -79,16 +86,25 @@ fun Greeting(name: String) {
             Column(
                 modifier = Modifier
                     .weight(1f)
-                    .padding(bottom = extraPadding)
+                    .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ) {
                 Text(text = "Hello,")
                 Text(text = name)
             }
             OutlinedButton(
-                onClick = { expanded.value = !expanded.value }
+                onClick = { expanded = !expanded }
             ) {
-                Text(if (expanded.value) "Show less" else "Show more")
+                Text(if (expanded) "Show less" else "Show more")
             }
+        }
+    }
+}
+
+@Composable
+private fun Greetings(names: List<String> = List(1000) { "$it" }) {
+    LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
+        items(items = names) { name ->
+            Greeting(name = name)
         }
     }
 }
