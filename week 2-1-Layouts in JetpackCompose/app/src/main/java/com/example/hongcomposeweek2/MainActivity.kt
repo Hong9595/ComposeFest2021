30d199ef189b5a92316b99f8f897c33b8225b983
Hong9595 <ahk959595@gmail.com>
2021-11-23 13:09:32 +0900

    Complex Custom Layout

--- week 2-1-Layouts in JetpackCompose/app/src/main/java/com/example/hongcomposeweek2/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/app/src/main/java/com/example/hongcomposeweek2/MainActivity.kt
@@ -19,6 +19,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.AlignmentLine
 import androidx.compose.ui.layout.FirstBaseline
 import androidx.compose.ui.layout.Layout
@@ -43,8 +44,106 @@ class MainActivity : ComponentActivity() {
 //                    LayoutsCodelab()
 //                    ScrollingList()
 //                    Text("Hi there!", Modifier.firstBaselineToTop(32.dp))
-                    BodyContentForMyOwnColumn()
+//                    BodyContentForMyOwnColumn()
+                    BodyContent3()
                 }
+            }
+        }
+    }
+}
+
+val topics = listOf(
+    "Arts & Crafts", "Beauty", "Books", "Business", "Comics", "Culinary",
+    "Design", "Fashion", "Film", "History", "Maths", "Music", "People", "Philosophy",
+    "Religion", "Social sciences", "Technology", "TV", "Writing"
+)
+
+
+@Composable
+fun BodyContent3(modifier: Modifier = Modifier) {
+    // StaggeredGridLayout이 화면 밖으로 벗어나게 되므로, scrollable한 Row로 감싼다.
+    Row(modifier = modifier.horizontalScroll(rememberScrollState())) {
+        StaggeredGridLayout {
+            for (topic in topics) {
+                Chip(modifier = Modifier.padding(8.dp), text = topic)
+            }
+        }
+    }
+//    StaggeredGridLayout(modifier = modifier) {
+//        for (topic in topics) {
+//            Chip(modifier = Modifier.padding(8.dp), text = topic)
+//        }
+//    }
+}
+
+@Composable
+fun Chip(modifier: Modifier = Modifier, text: String) {
+    Card(
+        modifier = modifier,
+        border = BorderStroke(color = Color.Black, width = Dp.Hairline),
+        shape = RoundedCornerShape(8.dp)
+    ) {
+        Row(
+            modifier = Modifier.padding(start = 8.dp, top = 4.dp, end = 8.dp, bottom = 4.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Box(
+                modifier = Modifier
+                    .size(16.dp, 16.dp)
+                    .background(color = MaterialTheme.colors.secondary)
+            )
+            Spacer(Modifier.width(4.dp))
+            Text(text = text)
+        }
+    }
+}
+
+@Composable
+fun StaggeredGridLayout(
+    modifier: Modifier = Modifier,
+    rows: Int = 3,
+    content: @Composable () -> Unit
+) {
+    Layout(
+        modifier = modifier,
+        content = content
+    ) { measurables, constraints ->
+        val rowWidths = IntArray(rows) { 0 }
+        val rowHeights = IntArray(rows) { 0 }
+
+        val placeables = measurables.mapIndexed { index, measurable ->
+            val placeable = measurable.measure(constraints)
+            // Track the width and max height of each row
+            val row = index % rows
+            // width는 accumulate, height는 max로 해주면 된다.
+            rowWidths[row] += placeable.width
+            rowHeights[row] = Math.max(rowHeights[row], placeable.height)
+            placeable
+        }
+
+        val width = rowWidths.maxOrNull()
+            ?.coerceIn(constraints.minWidth.rangeTo(constraints.maxWidth)) ?: constraints.minWidth
+        val height = rowHeights.sumOf { it }
+            .coerceIn(constraints.minHeight.rangeTo(constraints.maxHeight))
+
+        // Y of each row, based on the height accumulation of previous rows
+        val rowY = IntArray(rows) { 0 }
+        for (i in 1 until rows) {
+            rowY[i] = rowY[i-1] + rowHeights[i-1]
+        }
+
+        // Set the size of the parent layout
+        layout(width, height) {
+            // x cord we have placed up to, per row
+            val rowX = IntArray(rows) { 0 }
+
+            placeables.forEachIndexed { index, placeable ->
+                val row = index % rows
+                placeable.placeRelative(
+                    x = rowX[row],
+                    y = rowY[row]
+                )
+                rowX[row] += placeable.width
             }
         }
     }
